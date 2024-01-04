671973abc2279e17ee7fdf2668ba787d620d1874
MCzech3605 <mczech@student.agh.edu.pl>
2024-01-04 13:36:33 +0100

    Example file updated

--- src/main/resources/example.kt
+++ src/main/resources/example.kt
@@ -2,20 +2,20 @@ fun add(a: Int, b: Int): Int {
     return a + b
 }
 
-fun main(argCount: Int, args: String): Int {
+fun main(): Int {
     var a: Int = 5
     var b: Int = 10
     while (a <= b) {
-        print("Hello")
+        printf("Hello")
         a++
     }
 
-    if (argCount > 1)
+    if (a < 1)
         return 5
     else {
         a = 12
         b = 8
-        print(add(a, b))
+        var c : Int = add(a, b)
     }
     return 0
 }
